151f6b83a83b6555ebd65319ec3f757f45ba60d2
amalkjose2849 <mail@amalkjose.com>
2017-04-06 09:30:37 +0530

    Added new Drawable activity for showing contacts. (Contacts not added)

--- app/src/main/java/com/amalkjose/whatsappassistant/SheduleActivity.java
+++ app/src/main/java/com/amalkjose/whatsappassistant/SheduleActivity.java
@@ -29,8 +29,8 @@ public class SheduleActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent in=new Intent(SheduleActivity.this,SheduleContacts.class);
+                startActivity(in);
             }
         });
 
